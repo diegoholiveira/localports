c9cd61f6ac9cc063a3cb074da1ee03baeb4ad888
Diego Henrique Oliveira <contato@diegoholiveira.com>
2025-08-30 12:48:47 -0300

    Release v0.1.0

--- Formula/localports.rb
+++ Formula/localports.rb
@@ -1,8 +1,8 @@
 class Localports < Formula
   desc 'List network ports with their associated binaries'
   homepage 'https://github.com/diegoholiveira/localports'
-  url 'https://github.com/diegoholiveira/localports/archive/v1.0.0.tar.gz'
-  sha256 'REPLACE_WITH_ACTUAL_SHA256'
+  url 'https://github.com/diegoholiveira/localports/archive/v0.1.0.tar.gz'
+  sha256 'b1c96ed149ac41dfbf22d4b168e6ca61993effaf1dcb0a9da47bb30ee050e0a1'
   license 'MIT'
   head 'https://github.com/diegoholiveira/localports.git', branch: 'main'
 
